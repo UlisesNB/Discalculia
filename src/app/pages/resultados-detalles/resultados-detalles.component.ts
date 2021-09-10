740fe9e5fd26fef5e2d97fb122b56c0c6ac0b633
Ulises <unbenitez1994@gmail.com>
2021-09-10 15:28:25 -0400

    commit

--- src/app/pages/resultados-detalles/resultados-detalles.component.ts
+++ src/app/pages/resultados-detalles/resultados-detalles.component.ts
@@ -10,13 +10,20 @@ import { TestService } from '../../services/test.service';
 export class ResultadosDetallesComponent implements OnInit {
 
   id: any;
+  alumno_detalle: any;
+   
   constructor(private activatedRoute: ActivatedRoute, private testService: TestService) {
+    //Recuperamos el ID del objeto seleccionado para realizar la recuperacion de los datos correspondientes
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
     console.log('ID: ', this.id);
   }
 
   ngOnInit(): void {
+    /**
+     * Una vez obtendido el ID recuperamos los datos y lo almacenamos en una variable para su uso
+     */
     this.testService.getUserId(this.id).subscribe((data) => {
+      this.alumno_detalle = data;
       console.log('Resultado del ID: ', this.id, 'es: ', data);
     });
   }
